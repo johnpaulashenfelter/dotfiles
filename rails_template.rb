ab35e3a407ffc0ff5941bc9c97b50b1b21e953ba
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2018-07-09 19:08:57 -0400

    Configure shoulda in Rails 5.x template

--- rails_template.rb
+++ rails_template.rb
@@ -97,6 +97,7 @@ require File.expand_path("../../config/environment", __FILE__)
 abort("DATABASE_URL environment variable is set") if ENV["DATABASE_URL"]
 
 require "rspec/rails"
+require "shoulda/matchers"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
 
@@ -109,6 +110,13 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
 
 ActiveRecord::Migration.maintain_test_schema!
