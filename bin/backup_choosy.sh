1e390bb68df78e4fcb7b4f2b76be26ce1a7762c8
John Paul Ashenfelter <john@ashenfelter.com>
2021-12-29 17:34:37 -0500

    Migrate files to chezmoi dotfile manager
    This 'removes' all of the configuration files which are now managed
    in `chezmoi` instead of `rcm`. Those files will be added back in the
    next PR. In the meantime, here's what was migrated:
    
    migrate asdfrc
    
    Migrate bash, deprecated by zsh migration anyhow
    
    Migrate gem-related config
    
    Migrate git-related config
    
    Migrate editorconfig
    
    Migrate hushlogin
    
    Migrate rails config
    
    Migrate psql config
    
    Update README about chezmoi migration
    
    Migrate bin files
    
    Remove rcm related files
    
    Migrate zshrc
    
    Migrate remaining files to dotfiles in chezmoi

--- bin/backup_choosy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# from http://feedback.choosyosx.com/forums/6165-general/suggestions/12480483-add-export-import-of-rules-and-setting
-cp ~/Library/Application\ Support/Choosy/behaviours.plist ~/.dotfiles/settings/Application\ Support/Choosy/behaviours.plist
-
