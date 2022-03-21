e43e1c7fe8ccae3e7b2314a18965fbd1ce5f4b85
John Paul Ashenfelter <john@ashenfelter.com>
2022-03-21 17:23:13 -0400

    Add macos preferences script and notes inline about sources for more config

---
+++ home/.chezmoiscripts/run_once_after_92-macos-preferences.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ~/.macos — https://mths.be/macos
+# https://github.com/narze/dotfiles/blob/master/chezmoi/.chezmoiscripts/run_once_after_98-macos-preferences.sh
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
